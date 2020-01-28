716b5f221ef8e81e58e554f8b1517aed2455cf38
Priyam <priyam009@live.com>
2020-01-28 22:04:16 +1100

    Add server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+// Express
+var express = require("express");
+var path = require("path");
+
+// Set Port
+var PORT = process.env.PORT || 3000;
+
+// Initialise Express
+var app = express();
+
+// Require models
+var db = require("./models");
+
+//Static directory
+app.use(express.static(path.join(__dirname, "/public")));
+
+//Parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Express handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebar", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Require routes
+require("./controllers/html-routes")(app);
+require("./controllers/api-routes")(app);
+
+//Start server
+app.listen(PORT, function() {
+  console.log("Server listening on http://localhost:" + PORT);
+});
